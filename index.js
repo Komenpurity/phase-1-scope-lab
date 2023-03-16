5181a2d08d0d9da8203cfb005db81c1c65b5a8bb
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-16 09:59:58 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    let result = console.log(customerName.toUpperCase());
+    return result;
+} 
+upperCaseCustomerName();
+
+var bestCustomer = "not bob";
+
+const leastFavoriteCustomer= "mike";
+
+function setBestCustomer(){
+  let result = bestCustomer = "maybe bob";
+  return result;
+}
+setBestCustomer();
+
+
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+overwriteBestCustomer();
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "mark";
+}
+changeLeastFavoriteCustomer();
